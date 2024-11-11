04d91052ab7a04619dca6c241af0cda00d17aff7
Ayush N Shetty <126095015+AyuShetty@users.noreply.github.com>
2024-11-11 13:21:34 +0530

    Update codes.js

--- codes.js
+++ codes.js
@@ -31,134 +31,125 @@ for i, j in enumerate(np.unique(y_set)):
 plt.legend()
 plt.show()`,
 
-    button2: `import socket
+    button2: `import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('localhost', 7999))
-print("Server is running")
-while True:
-    data, addr = s.recvfrom(1024)
-    msg = data.decode().upper()
-    s.sendto(msg.encode(), addr)
+df = pd.read_csv('Groceries_dataset.csv')
+df.drop_duplicates(inplace=True)
+combinacoes = df.groupby(['Member_number', 'Date']).agg({'itemDescription': list}).reset_index()
+combinacoes_list = combinacoes['itemDescription'].tolist()
 
+te = TransactionEncoder()
+combinacoes_bool = te.fit_transform(combinacoes_list)
+dataset = pd.DataFrame(combinacoes_bool, columns=te.columns_)
+regras = apriori(dataset, min_support=0.001, use_colnames=True)
+associacao_regras = association_rules(regras, metric='confidence', min_threshold=0.1)
+associacao_regras.sort_values(['confidence'], ascending=False)`,
 
-import socket
+    button3: `import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_addr = ('localhost', 7999)
-while True:
-    msg = input("Enter message (or 'END' to stop): ")
-    if msg == "END":
-        break
-    s.sendto(msg.encode(), server_addr)
-    print("Server:", s.recv(1024).decode())
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.show()
 
-s.close()`,
-
-    button3: `import time
-import random
-
-def leaky_bucket(pkt_size, output_rate):
-    if pkt_size > 512:
-        print("Bucket overflow")
-    else:
-        while pkt_size > output_rate:
-            print(f"{output_rate} bytes sent")
-            pkt_size -= output_rate
-            time.sleep(0.1)
-        if pkt_size > 0:
-            print(f"{pkt_size} bytes sent")
-
-output_rate = int(input("Enter output rate: "))
-n = int(input("Enter number of packets: "))
-
-for i in range(n):
-    pkt_size = random.randint(1, 1000)
-    print(f"Packet {i+1} size: {pkt_size}")
-    leaky_bucket(pkt_size, output_rate)`,
+kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
+y_kmeans = kmeans.fit_predict(X)
+for i in range(5):
+    plt.scatter(X[y_kmeans == i, 0], X[y_kmeans == i, 1], s=100, label=f'Cluster {i+1}')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
+plt.legend()
+plt.show()`,
         
-    button4: `import socket
+    button4: `import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy.cluster.hierarchy as sch
+from sklearn.cluster import AgglomerativeClustering
 
-s = socket.socket()
-s.bind(('localhost', 2111))
-s.listen(1)
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.show()
 
-conn, _ = s.accept()
-
-while True:
-    data = conn.recv(1024).decode() 
-    if data.lower() == "goodbye":
-        break   
-    print("Client:", data)    
-    reply = input("Reply: ")   
-    conn.send(reply.encode())
-conn.close()
-
-import socket
-
-s = socket.socket()
-s.connect(('localhost', 2111))
-
-while True:
-    msg = input("Message to server: ")
-    s.send(msg.encode())
-    if msg.lower() == "goodbye":
-        break
-    print("Server:", s.recv(1024).decode())
-s.close()`,
+hc = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='ward')
+y_hc = hc.fit_predict(X)
+for i in range(5):
+    plt.scatter(X[y_hc == i, 0], X[y_hc == i, 1], s=100, label=f'Cluster {i+1}')
+plt.legend()
+plt.show()`,
         
-    button5: `//RECEIVER
-import socket
+    button5: `import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from matplotlib.colors import ListedColormap
 
-s = socket.socket()
-s.bind(('localhost', 6869))
-s.listen(1)
-conn, _ = s.accept()
-data = conn.recv(1024).decode()
-unstuffed = data[8:-8].replace('111110', '11111')
-print("Unstuffed data:", unstuffed)
-conn.close()
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+accuracy_score(y_test, y_pred)
 
-//SENDER
-import socket
-
-s = socket.socket()
-s.connect(('localhost', 6869))
-data = input("Enter binary data: ")
-stuffed = "01111110" + data.replace('11111', '111110') + "01111110"
-s.send(stuffed.encode())
-print("Stuffed data sent:", stuffed)
-s.close()`,
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min()-10, stop=X_set[:, 0].max()+10, step=0.25),
+                     np.arange(start=X_set[:, 1].min()-1000, stop=X_set[:, 1].max()+1000, step=0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
+plt.legend()
+plt.show()`,
     
-    button6: `n = int(input("Enter number of nodes: "))
-dmat = [[int(input(f"Cost between node {i+1} and node {j+1}: ")) for j in range(n)] for i in range(n)]
-dist = [dmat[i][:] for i in range(n)]
+    button6: `import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
-for _ in range(n-1):
-    for i in range(n):
-        for j in range(n):
-            dist[i][j] = min(dist[i][j], min(dist[i][k] + dist[k][j] for k in range(n)))
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
-print("Distance table:")
-for i, row in enumerate(dist):
-    print(f"Node {i+1}: {row}")`,
+ann = tf.keras.models.Sequential()
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+ann.fit(X_train, y_train, batch_size=32, epochs=100)
+
+y_pred = (ann.predict(X_test) > 0.5)
+cm = confusion_matrix(y_test, y_pred)`,
     
-    button7: `def xor(a, b):
-    return ''.join('0' if i == j else '1' for i, j in zip(a, b))
 
-def crc(data, gen):
-    code = data + '0' * (len(gen) - 1)
-    for i in range(len(data)):
-        if code[i] == '1':
-            code = code[:i] + xor(code[i:i+len(gen)], gen) + code[i+len(gen):]
-    return code[-(len(gen)-1):]
-
-data = input("Enter data: ")
-gen = input("Enter generator polynomial: ")
-transmitted = data + crc(data, gen)
-print("Transmitted code:", transmitted)
-
-received = input("Enter received code to check for errors: ")
-print("No errors" if crc(received, gen) == '0' * (len(gen)-1) else "Errors detected")
-`
 };
